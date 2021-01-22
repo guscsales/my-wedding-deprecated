7a3b9290a5391fcbad44d306525ddc6aed4360c0
Gustavo Sales <gustavo@useorigin.com>
2021-01-22 20:38:03 -0300

    transform in only assets project

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,24 @@ const gulpUglify = require('gulp-uglify');
 const gulpUglifyCss = require('gulp-uglifycss');
 const pipeline = require('readable-stream').pipeline;
 
-const copy = () => gulp.src(['public/**/*']).pipe(gulp.dest('dist/public'));
+const copy = () => gulp.src(['src/**/*']).pipe(gulp.dest('dist'));
 
 const uglifyScripts = () =>
 	pipeline(
-		gulp.src('public/js/*.js'),
+		gulp.src('src/js/*.js'),
 		gulpUglify(),
-		gulp.dest('dist/public/js')
+		gulp.dest('dist/js')
 	);
 
 const uglifyStyles = () =>
 	gulp
-		.src('./public/css/*.css')
+		.src('./src/css/*.css')
 		.pipe(
 			gulpUglifyCss({
 				maxLineLen: 80,
 				uglyComments: true
 			})
 		)
-		.pipe(gulp.dest('./dist/public/css'));
+		.pipe(gulp.dest('./dist/css'));
 
 exports.default = gulp.series(copy, uglifyScripts, uglifyStyles);
